44bb90773e1aa7ead1faae026d9a49640a74f2b3
Shynggys Shynbolatov <shyngys.shynbolatov@gmail.com>
2023-06-12 17:26:42 +0700

    initial run added in cron

--- cron.ts
+++ cron.ts
@@ -7,6 +7,7 @@ import alert_new_follow from "/watchers/alert_new_follow.ts";
 
 const every6hour  = "1 */6 * * *";
 
+await alert_new_follow();
 cron(every6hour, async () => {
   logger.info("cron job every 6 hours");
   await alert_new_follow();
